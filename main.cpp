5e64e91f5d0ccadeeb1aa682202b936d398f214c
Symbolic <sym12138@hotmail.com>
2023-11-30 01:57:42 +0800

    add some vcpkg config for libigl.

--- main.cpp
+++ main.cpp
@@ -1,15 +1,16 @@
-#include <igl/cotmatrix.h>
+#include <igl/readOFF.h>
+#include <igl/opengl/glfw/Viewer.h>
 
-#include <Eigen/Dense>
-#include <Eigen/Sparse>
-#include <iostream>
-int main() {
-    Eigen::MatrixXd V(4, 2);
-    V << 0, 0, 1, 0, 1, 1, 0, 1;
-    Eigen::MatrixXi F(2, 3);
-    F << 0, 1, 2, 0, 2, 3;
-    Eigen::SparseMatrix<double> L;
-    igl::cotmatrix(V, F, L);
-    std::cout << "Hello, mesh: " << std::endl << L * V << std::endl;
-    return 0;
+Eigen::MatrixXd V;
+Eigen::MatrixXi F;
+
+int main(int argc, char *argv[])
+{
+  // Load a mesh in OFF format
+  igl::readOFF(TUTORIAL_SHARED_PATH "/bunny.off", V, F);
+
+  // Plot the mesh
+  igl::opengl::glfw::Viewer viewer;
+  viewer.data().set_mesh(V, F);
+  viewer.launch();
 }
