3490c644aa5c7202a6a91721ee8f721c2ae26b7b
hundan2015 <sym12138@hotmail.com>
2023-11-30 21:53:37 +0800

    fully fix dependency error in windows.

--- main.cpp
+++ main.cpp
@@ -1,16 +1,15 @@
-#include <igl/readOFF.h>
 #include <igl/opengl/glfw/Viewer.h>
+#include <igl/readOFF.h>
 
 Eigen::MatrixXd V;
 Eigen::MatrixXi F;
 
-int main(int argc, char *argv[])
-{
-  // Load a mesh in OFF format
-  igl::readOFF("/bunny.off", V, F);
+int main(int argc, char *argv[]) {
+    // Load a mesh in OFF format
+    igl::readOFF("/bunny.off", V, F);
 
-  // Plot the mesh
-  igl::opengl::glfw::Viewer viewer;
-  viewer.data().set_mesh(V, F);
-  viewer.launch();
+    // Plot the mesh
+    igl::opengl::glfw::Viewer viewer;
+    viewer.data().set_mesh(V, F);
+    viewer.launch();
 }
