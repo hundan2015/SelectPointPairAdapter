56cc7b46f2b1bca7ef29af2f15bb24be4bfe8c19
Symbolic <sym12138@hotmail.com>
2023-12-01 11:32:22 +0800

    now it can process basic function.

--- main.cpp
+++ main.cpp
@@ -1,20 +1,22 @@
 #include <igl/barycentric_coordinates.h>
 #include <igl/opengl/glfw/Viewer.h>
 #include <igl/point_mesh_squared_distance.h>
-#include <igl/slice.h>
+
+#include <iostream>
+#include <ostream>
+#include <vector>
 
 #include "igl/readOFF.h"
+
 using namespace Eigen;
+using std::cout;
+using std::vector;
+
 int main(int argc, char *argv[]) {
     Eigen::MatrixXd inputModelVertices;
     Eigen::MatrixXi inputModelFaces;
     // Load a mesh in OFF format
     igl::readOFF("./dd.off", inputModelVertices, inputModelFaces);
-
-    // Plot the mesh
-    igl::opengl::glfw::Viewer viewer;
-    viewer.data().set_mesh(inputModelVertices, inputModelFaces);
-    viewer.launch();
 
     Eigen::MatrixXd inputPointsVertices;
     Eigen::MatrixXi inputPointsFaces;
@@ -37,4 +39,30 @@ int main(int argc, char *argv[]) {
     igl::slice(inputModelVertices, F2.col(1), xyz, Vb);
     igl::slice(inputModelVertices, F2.col(2), xyz, Vc);
     igl::barycentric_coordinates(closetPointFromP2Mesh, Va, Vb, Vc, L);
+    std::cout << L;
+
+    cout << std::endl;
+    // Make select pair part
+    int size = elementI.size();
+    vector<vector<double>> shit;
+    for (int i = 0; i < L.rows(); ++i) {
+        vector<double> temp;
+        temp.push_back(elementI(i));
+        temp.push_back(L(i, 0));
+        temp.push_back(L(i, 1));
+        shit.push_back(temp);
+    }
+
+    for (int i = 0; i < shit.size(); ++i) {
+        for (int j = 0; j < 3; ++j) {
+            cout << shit[i][j] << " ";
+        }
+        cout << std::endl;
+    }
+
+    // Plot the mesh
+    igl::opengl::glfw::Viewer viewer;
+    viewer.data().set_mesh(inputModelVertices, inputModelFaces);
+    viewer.data().add_points(inputPointsVertices, Eigen::RowVector3d(1, 0, 0));
+    viewer.launch();
 }
