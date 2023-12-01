dd65a6dafce6e7b2518531c22c8bad1443814e5a
hundan2015 <sym12138@hotmail.com>
2023-12-01 22:53:59 +0800

    double engined version. But not arg friendly.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
-#include <igl/barycentric_coordinates.h>
+// #include <igl/barycentric_coordinates.h>
 #include <igl/opengl/glfw/Viewer.h>
 #include <igl/point_mesh_squared_distance.h>
+#include <igl/readOBJ.h>
 
 #include <args.hxx>
 #include <iostream>
 #include <ostream>
+#include <tuple>
 #include <vector>
+#include <nlohmann/json.hpp>
+using json = nlohmann::json;
 
 #include "igl/readOFF.h"
 
@@ -13,37 +17,11 @@ using namespace Eigen;
 using std::cout;
 using std::vector;
 
-int main(int argc, char* argv[]) {
-    args::ArgumentParser parser(
-        "A simple program for wrap4d coordinates transfer.");
-    args::HelpFlag(parser, "help",
-                   "If you need help, check below:", {'h', "help"});
-    args::Group group(
-        parser, "This group is all exclusive:", args::Group::Validators::AllOrNone);
-    args::Flag isUsingGUI(group, "gui", "The GUI flag.", {'g', "gui"});
-    try {
-        parser.ParseCLI(argc, argv);
-    } catch (const args::Completion& e) {
-        std::cout << e.what();
-        return 0;
-    } catch (const args::ParseError& e) {
-        std::cerr << e.what() << std::endl;
-        std::cerr << parser;
-        return 1;
-    } catch (const args::Help&) {
-        std::cout << parser;
-        return 0;
-    }
-
-    Eigen::MatrixXd inputModelVertices;
-    Eigen::MatrixXi inputModelFaces;
+void process_barycentric(const Eigen::MatrixXd& inputModelVertices,
+                         const Eigen::MatrixXi& inputModelFaces,
+                         const Eigen::MatrixXd& inputPointsVertices) {
     // Load a mesh in OFF format
-    igl::readOFF("./dd.off", inputModelVertices, inputModelFaces);
-
-    Eigen::MatrixXd inputPointsVertices;
-    Eigen::MatrixXi inputPointsFaces;
-
-    igl::readOFF("./inputPoints.off", inputPointsVertices, inputPointsFaces);
+    // igl::readOFF("./dd.off", inputModelVertices, inputModelFaces);
 
     Eigen::VectorXd squaredDistanceFromP2Mesh;
     Eigen::VectorXi elementI;
@@ -61,29 +39,88 @@ int main(int argc, char* argv[]) {
     igl::slice(inputModelVertices, F2.col(1), xyz, Vb);
     igl::slice(inputModelVertices, F2.col(2), xyz, Vc);
     igl::barycentric_coordinates(closetPointFromP2Mesh, Va, Vb, Vc, L);
-    std::cout << L;
 
     cout << std::endl;
     // Make select pair part
     int size = elementI.size();
-    vector<vector<double>> shit;
+    vector<std::tuple<int, double, double>> shit;
     for (int i = 0; i < L.rows(); ++i) {
-        vector<double> temp;
-        temp.push_back(elementI(i));
-        temp.push_back(L(i, 0));
-        temp.push_back(L(i, 1));
-        shit.push_back(temp);
+        shit.push_back({elementI(i), L(i, 0), L(i, 1)});
+    }
+    // Output part
+    json jVector(shit);
+    cout << jVector;
+}
+
+
+// Struct to transfer off to json easily
+struct vertex {
+    double x;
+    double y;
+    double z;
+};
+NLOHMANN_DEFINE_TYPE_NON_INTRUSIVE(vertex, x, y, x);
+void transfer_off_json(const Eigen::MatrixXd& inputPointsVertices) {
+    vector<vertex> result;
+    for (int i = 0; i < inputPointsVertices.cols(); ++i) {
+        result.push_back({inputPointsVertices(i, 0), inputPointsVertices(i, 1),
+                          inputPointsVertices(i, 2)});
+    }
+    json jVector(result);
+    // Output part
+    cout << jVector;
+}
+
+int main(int argc, char* argv[]) {
+    args::ArgumentParser parser(
+        "A simple program for wrap4d coordinates transfer.");
+    args::HelpFlag(parser, "help",
+                   "If you need help, check below:", {'h', "help"});
+    args::Group guiGroup(parser,
+                         "If you use -g, you can view the points in imgui:",
+                         args::Group::Validators::AllOrNone);
+    args::Flag isUsingGUI(guiGroup, "gui", "The GUI flag.", {'g', "gui"});
+
+    args::Group engineGroup(
+        parser,
+        "If you use this group, you are using the engine provided by me:");
+    args::Flag isUsingInsertedEngine(
+        engineGroup, "engine", "The inserted engine flag", {'e', "engine"});
+
+    try {
+        parser.ParseCLI(argc, argv);
+    } catch (const args::Completion& e) {
+        std::cout << e.what();
+        return 0;
+    } catch (const args::ParseError& e) {
+        std::cerr << e.what() << std::endl;
+        std::cerr << parser;
+        return 1;
+    } catch (const args::Help&) {
+        std::cout << parser;
+        return 0;
     }
 
-    for (int i = 0; i < shit.size(); ++i) {
-        for (int j = 0; j < 3; ++j) {
-            cout << shit[i][j] << " ";
-        }
-        cout << std::endl;
+    // Setup input mesh and points matrix
+    Eigen::MatrixXd inputModelVertices;
+    Eigen::MatrixXi inputModelFaces;
+    Eigen::MatrixXd inputPointsVertices;
+    Eigen::MatrixXi inputPointsFaces;
+    // Load Meshes
+    if (isUsingGUI || isUsingInsertedEngine) {
+        igl::readOBJ("./input.obj", inputModelVertices, inputModelFaces);
+    }
+    igl::readOFF("./input.off", inputPointsVertices, inputPointsFaces);
+
+    if (isUsingInsertedEngine) {
+        process_barycentric(inputModelVertices, inputModelFaces,
+                            inputPointsVertices);
+    } else {
+        transfer_off_json(inputPointsVertices);
     }
 
-    if (isUsingGUI) {  
-    // Plot the mesh
+    if (isUsingGUI) {
+        // Plot the mesh
         igl::opengl::glfw::Viewer viewer;
         viewer.data().set_mesh(inputModelVertices, inputModelFaces);
         viewer.data().add_points(inputPointsVertices,
